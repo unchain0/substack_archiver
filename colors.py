287bcca3bae869f08fcbfb37ee9e9511dceec8dc
iptux <>
2023-03-20 23:31:05 +0100

    CLI Colors, Error handling

---
+++ colors.py
@@ -0,0 +1,26 @@
+from termcolor import colored
+from pyfiglet import figlet_format
+import six
+
+def log(string, color, font="slant", figlet=False):
+    if colored:
+        if not figlet:
+            six.print_(colored(string, color))
+        else:
+            six.print_(colored(figlet_format(
+                string, font=font), color))
+    else:
+        six.print_(string)
+
+
+def welcome_message():
+    log("Substack Archiver", "green", "slant", True)
+    log("Created by: @VitaminCPU", "green")
+    log("Version: 1.0", "green")
+    log("Github: github.com/pwrtux", "green")
+    print("")
+    log("========================================", "white")
+    log("Welcome to the Substack Archiver", "white")
+    log("========================================", "white")
+    print("")
+    
--- substack_archiver.py
+++ substack_archiver.py
@@ -5,24 +5,52 @@ import json
 import re
 import os
 from dotenv import load_dotenv
+import sys
+import colors
 
 load_dotenv("config.env")
 
 USERNAME = os.environ.get('SUBSTACK_USERNAME')
 PASSWORD = os.environ.get('SUBSTACK_PASSWORD')
-os.mkdir("html_dumps")
-os.mkdir("json_dumps")
+
+
+def command_line_parser():
+    if len(sys.argv) != 3:
+        colors.log("""Usage: python3 substack_archiver.py "SUBSSTACKNAME" "URL", """, "white")
+        colors.log("""Example: python3 substack_archiver.py "abc" "https://abc.substack.com" or with custom domain: "name123" "https://abc.com" """, "white")
+        print("")
+        sys.exit(1)
+    else:
+        return sys.argv[1], sys.argv[2]
+
+
 
 class SubstackAPIHandler:
 
-    def __init__(self, username, password, cookies=None, post_requests=None):
+    def __init__(self, username, password, substack_handle, base_url, cookies=None, post_requests=None):
+
+
         self.username = username
         self.password = password
-        self.login_url = "https://public.substack.com/api/v1/login"
+        self.substack_handle = substack_handle
+        self.base_url = base_url
+        self.login_url = f"{self.base_url}/api/v1/login" 
+        #self.post_url = f"https://{substack_handle}.substack.com/api/v1/posts/?limit=50&offset=" # what if custom domain?
+        self.post_url = f"{self.base_url}/api/v1/posts?limit=50&offset="
         self.cookies = cookies
         self.post_requests = post_requests
+        # html_dumps and json_dumps folder path
+        self.html_path = f"html_dumps/{substack_handle}"
+        self.json_path = f"json_dumps/{substack_handle}"  
 
-    
+        
+        try:
+            os.mkdir(f"html_dumps/{substack_handle}")
+            os.mkdir(f"json_dumps/{substack_handle}")
+        except FileExistsError:
+            colors.log("Folder already exists", "red")
+            raise FileExistsError("Folder already exists.")
+        
 
 
     def login(self):
@@ -38,20 +66,23 @@ class SubstackAPIHandler:
         response = requests.post(url=self.login_url, data=payload)
 
         if response.status_code != 200:
-            raise ValueError("Login failed. Please check your credentials")
+            colors.log("Login failed", "red")
+            raise ValueError("Login failed. Please check your credentials or the Substack URL.")
         if response.status_code == 200:
-            print("Login successful")
+            colors.log("Login successful", "green")
+            print("")
+            colors.log("Fetching posts...", "white")
+            print("")
+
 
         self.cookies = response.cookies
      
-
-    
 
     def get_posts(self):
         post_requests = []
         # iterate. max. 50 posts per page. increase offset +50 in each iteration
         for i in range(0, 500, 50):
-            post_url = f"https://public.substack.com/api/v1/posts/?limit=50&offset={i}"
+            post_url = f"{self.post_url}{i}"
             get_posts = requests.request("GET", post_url, cookies=self.cookies).json()
             if get_posts == []:
                 break
@@ -62,41 +93,72 @@ class SubstackAPIHandler:
 
     def dump_to_json(self):
         for i in range(len(self.post_requests)):
-            with open(f"./json_dumps/dump{i}.json", "w") as f:
+            with open(f"{self.json_path}/dump{i}.json", "w") as f:
                 json.dump(self.post_requests[i], f)
         return
 
-    def serialize(value):
-        value = re.sub(r"[^\w\s-]", "", value.lower())
-        return re.sub(r"[-\s]+", "-", value).strip("-_")
+    def serialize(value): 
+        value = str(value)
+        value = re.sub(r"[^\w\s-]", "", value)
+        return re.sub(r"[-\s]+", "-", value).replace("-", " ")
 
 
     def parse_to_html(self):
         all_titles = []
         all_bodies = []
         all_descriptions = []
+        body_none = []
 
         # Parse to multiple HTML files
         for post_request in self.post_requests:
             for post in post_request:
                 all_titles.append(post["title"])
-                all_bodies.append(post["body_html"])
                 all_descriptions.append(post["description"])
+                if post["body_html"] is None:
+                    body_none.append(post["title"])
+                else:   
+                    all_bodies.append(post["body_html"])
+                
 
         for title, body, desciption in zip(all_titles, all_bodies, all_descriptions):
+            if body is None:
+                continue
             html_template = f"""<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>{title}</title> </head> <body> <h1>{title}</h1> <p> {desciption} </p> <p>{body}</p> </body> </html>"""
             file_name = SubstackAPIHandler.serialize(title) 
-            with open(f"./html_dumps/{file_name}.html", "wb") as f:
+            with open(f"{self.html_path}/{file_name}.html", "wb") as f:
                 f.write(html_template.encode("utf-8"))
+
+        return body_none, all_titles
         
 
 
 
 
 if __name__ == "__main__":
-    api_handler = SubstackAPIHandler(USERNAME, PASSWORD)
+
+    if USERNAME == "" or PASSWORD == "":
+        print("Please set the SUBSTACK_USERNAME and SUBSTACK_PASSWORD environment variables")
+        sys.exit(1)
+
+    
+    colors.welcome_message()
+
+    substack_handle = command_line_parser()
+    api_handler = SubstackAPIHandler(USERNAME, PASSWORD, substack_handle[0], substack_handle[1])
+    
+
+
     api_handler.login()
     api_handler.get_posts()
     api_handler.dump_to_json()
-    api_handler.parse_to_html()
+    html = api_handler.parse_to_html()
+
+    colors.log(f"Number of downloaded posts: {len(html[1])}", "green")
+    print("")
+
+    if len(html[0]) != 0:
+        colors.log(f"Number of Inaccessible Posts: {len(html[0])}", "red")
+        colors.log(f"Probably not a paid subscriber. Are you using a paid subscription account?", "red")
         
+    colors.log("Done!", "green", "bold")
+    print("")
