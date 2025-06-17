5b6ca0b136f8988beb40a4af4495289d4c6daa50
Felippe A. <unchain0@proton.me>
2025-06-17 17:36:20 -0300

    Add type hints, refactor the `log` function, and enhance print handling

--- colors.py
+++ colors.py
@@ -2,18 +2,20 @@ from termcolor import colored
 from pyfiglet import figlet_format
 import six
 
-def log(string, color, font="slant", figlet=False):
-    if colored:
-        if not figlet:
-            six.print_(colored(string, color))
-        else:
-            six.print_(colored(figlet_format(
-                string, font=font), color))
-    else:
-        six.print_(string)
+
+def log(string: str, color: str, font="slant", figlet=False) -> None:
+    if colored is None:
+        six.print_(string, flush=True)
+        return
+
+    if not figlet:
+        six.print_(colored(string, color), flush=True)
+        return
+
+    six.print_(colored(figlet_format(string, font=font), color), flush=True)
 
 
-def welcome_message():
+def welcome_message() -> None:
     log("Substack Archiver", "green", "slant", True)
     log("Created by: @VitaminCPU", "green")
     log("Version: 2.0", "green")
@@ -23,4 +25,3 @@ def welcome_message():
     log("Welcome to the Substack Archiver", "white")
     log("========================================", "white")
     print("")
-    
