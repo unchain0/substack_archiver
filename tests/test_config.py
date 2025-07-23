723630e86cb129a397ee3b59bda43dac280b4668
Felippe Andrade <unchain0@proton.me>
2025-07-23 04:52:43 -0300

    refactor: Update tests to use a separate test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,11 +5,11 @@ from app.config import load_config
 
 def test_load_config():
     config_data = [{"name": "test", "url": "https://test.substack.com"}]
-    config_file = Path("config.json")
+    config_file = Path("config-test.json")
     with open(config_file, "w") as f:
         json.dump(config_data, f)
 
-    config = load_config()
+    config = load_config(str(config_file))
     assert config == config_data
 
     config_file.unlink()
